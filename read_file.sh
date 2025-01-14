656f75bf434c1dee679f73a0144c01c3c643dde2
Isioma Osawele <isiomaosawele@gmail.com>
2025-01-14 18:08:16 +0100

    new commits

--- read_file.sh
+++ read_file.sh
@@ -8,3 +8,16 @@ exec 10<&0
 
 #Let the stdin (0) accept the first file given to it as it's input file
 exec < $1
+
+let count=0
+
+while read LINE; do
+    ARRAY[$count]=$LINE
+    ((count++))
+done
+
+echo Number of Elements: ${#ARRAY[@]}
+echo Array Content: ${ARRAY[@]}
+
+#return file_descriptor back to it's default (0) and destroy current file_descriptor
+exec 0<&10 10<&- 
